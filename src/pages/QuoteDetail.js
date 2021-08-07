7d293a02abe2caabaffa584c95f256dc1f2908a3
Zdravko Sosa <zdravko.sosa@gmail.com>
2021-08-07 19:48:34 +0200

    Added dummy data

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,15 +1,26 @@
 import { Fragment } from 'react';
 import { useParams, Route } from 'react-router-dom';
 
+import HighLightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Zdravko', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Sosa', text: 'Learning React is great!' },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
     <Fragment>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighLightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
