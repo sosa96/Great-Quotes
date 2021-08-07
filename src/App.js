6ad61488a5468708f01dc21eb5ae10d3fb6ddb35
Zdravko Sosa <zdravko.sosa@gmail.com>
2021-08-07 19:52:45 +0200

    Added Not Found page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AllQuotes from './pages/AllQuotes';
 import QuoteDetail from './pages/QuoteDetail';
 import NewQuote from './pages/NewQuote';
 import Layout from './components/layout/Layout';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -20,6 +21,10 @@ function App() {
         </Route>
         <Route path='/new-quote'>
           <NewQuote />
+        </Route>
+        (// Matches all URLs)
+        <Route path='*'>
+          <NotFound />
         </Route>
       </Switch>
     </Layout>
